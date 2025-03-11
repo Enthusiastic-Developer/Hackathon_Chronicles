164725bd617a532759264e56a849ffd79520c69e
NikhilDevuse <nikhil.devuse404@outlook.com>
2025-03-11 12:10:52 +0530

    Form Wave completed

--- Challenge/Form Wave/script.js
+++ Challenge/Form Wave/script.js
@@ -1,1 +1,11 @@
-// Add your JS for Form Wave here
+const labels = document.querySelectorAll(".form-control label");
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split("")
+    .map(
+      (letter, idx) =>
+        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
+    )
+    .join("");
+});
